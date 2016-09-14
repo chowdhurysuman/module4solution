a4968ca759d865dd52c96d22c66715a4f3c656c1
chowdhurysuman <chowdhurysuman22@gmail.com>
2016-09-15 01:44:37 +0530

    create SpeakHello

---
+++ SpeakHello.js
@@ -0,0 +1,8 @@
+(function (window) {
+	var helloSpeaker = {};
+	var speakWord1 = "Hello";
+	helloSpeaker.speak = function(name) {
+    console.log(speakWord1 + " " +names[i]);
+    }
+  window.helloSpeaker = helloSpeaker;
+  })(window);
